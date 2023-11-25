75ea2557a3a141bea7885df9ae095c0b44b0d78d
Addala M. Raed <addala.raed@gmail.com>
2023-11-25 01:24:18 +0100

    Update AC_solution.cpp

--- Problem_01/AC_solution.cpp
+++ Problem_01/AC_solution.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main() {
-    int t; cin >> t;
-    while(t--) {
-
-        int n, m; cin >> n >> m;
-
+    string s;
+    string msg = "NO";
+    cin >> s;
+    for(int i = 0 ; i < s.size(); ++i){
+        if(s[i] == 'W')
+        {
+            msg = "YES";
+            break;
+        }
     }
+    cout << msg;
     return 0;
 }
