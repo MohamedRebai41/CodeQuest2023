fd7f7e50cb9593dd3d86a330c0cb5f024008ce5e
AminaGhannem <amina.ghannem@insat.ucar.tn>
2023-11-30 18:44:50 +0100

    added python and java solutions

---
+++ Problem_08/AC_solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int k = scanner.nextInt();
+            int res = 0;
+            if ((k + y - 1) % x == 0) {
+                res = (k + y - 1) / x;
+            } else {
+                res = (k + y - 1) / x + 1;
+            }
+            System.out.println(res);
+        }
+    }
+}
