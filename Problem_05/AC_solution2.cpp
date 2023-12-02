8b478b3d818f63432b1c6c58fb066dd10180ba56
Mohamed Rebai <mohamedrebai748@gmail.com>
2023-12-02 12:26:49 +0100

    fixed second solution

--- Problem_05/AC_solution2.cpp
+++ Problem_05/AC_solution2.cpp
@@ -15,24 +15,28 @@ int extEuclidAlgo(int a, int b, int& x, int& y) {
 }
 
 int main() {
-
-    int occ = 0;
-    int n, x0, y0, x, y, k;
-    cin >> n;
+    int x0, y0, x, y, k;
 //    1 = 11 * x0 + 111 * y0
     extEuclidAlgo(11, 111, x0, y0);
 //    n = 11 * x + 111 * y
-    x = x0 * n;
-    y = y0 * n;
-//    general solution
-//    x = x0 + 111 * k
-//    y = y0 - 11 * k;
-    k = (n / 11);
-//    trying to make x >= 0 and y >= 0
-    x += k * 111;
-    y -= k * 11;
-    if (x >= 0 && y >= 0) cout << "YES" << endl;
-    else cout << "NO" << endl;
+    int t; cin >> t;
+    while(t--)
+    {
+        int n; cin >> n;
+        x = x0 * n;
+        y = y0 * n;
+        //    general solution
+        //    x = x0 + 111 * k
+        //    y = y0 - 11 * k;
+        k = (n / 11);
+        //    trying to make x >= 0 and y >= 0
+        x += k * 111;
+        y -= k * 11;
+        if (x >= 0 && y >= 0) cout << "YES" << endl;
+        else cout << "NO" << endl;
 
+
+    }
     return 0;
+    
 }
