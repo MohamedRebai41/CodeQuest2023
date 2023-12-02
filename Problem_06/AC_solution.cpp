eec819b271850c14418fbad5ff41f0897ec7b009
Iyed <iyedabdelli73@gmail.com>
2023-12-02 16:30:01 +0100

    Update accepted solution

--- Problem_06/AC_solution.cpp
+++ Problem_06/AC_solution.cpp
@@ -1,34 +1,29 @@
-#include<bits/stdc++.h>  
+#include<bits/stdc++.h>
 using namespace std;
 
-int matrix[1001][1001];
-// this prefix arrays must be long long because numbers are so large (up to 10^9)
-long long prefixRows[1001][1001]; // this serves as a prefix sum for the rows; prefixRows[i][j] = prefix sum in column i for the row j
-long long prefixColumns[1001][1001]; // this serves as a prefix sum for the columns; prefixColumns[i][j] = prefix sum in row i for the column j
+// Arrays must be long long because integers between -10^9 and 10^9
+long long matrix[1002][1002];
+long long prefixSum[1002][1002];
 
 int main() {
     int n, m, q; cin >> n >> m >> q;
     for (int i = 1; i <= n; i++){
-        for (int i = 1; j <= m; j++){
+        for (int j = 1; j <= m; j++){
             cin >> matrix[i][j];
         }
     }
-    // Calculating the prefix sum for rows and columns
-    for (int i = 1; i <= m; i++){
-        for (int j = 1; j <= n; j++){
-            prefixRows[i][j] = prefixRows[i][j - 1] + matrix[i][j];
-        }
-    }
+
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= m; j++){
-            prefixColumns[i][j] = prefixColumns[i][j - 1] + matrix[i][j];
+            prefixSum[i][j] = matrix[i][j] + prefixSum[i - 1][j - 1];
         }
     }
 
     for (int i = 0; i < q; i++){
         int x, y;
         cin >> x >> y;
-        if (prefixRows[y][x - 1] + prefixColumns[x][y - 1] == (prefixRows[y][n] - prefixRows[y][x] + prefixColumns[x][m] - prefixColumns[x][y])){
+        // min(n - x, m - y) + x and min(n - x, m - y) + y denotes the position of last element in the diagonal associated to x, y
+        if (prefixSum[x - 1][y - 1] == prefixSum[min(n - x, m - y) + x][min(n - x, m - y) + y] - prefixSum[x][y]){
             cout << "YES" << endl;
         }
         else{
