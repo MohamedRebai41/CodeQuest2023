23c2dbb48c038a3a9b30aaed17fd2d216c0c8d07
mohamedRebai <mohamedrebai748@gmail.com>
2023-12-11 23:25:47 +0100

    updated problem 9

--- Problem_09/AC_solution.java
+++ Problem_09/AC_solution.java
@@ -1,79 +1,76 @@
-package Problem_09;
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AC_solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
-        
+
         for (int testCase = 0; testCase < t; testCase++) {
             int n = scanner.nextInt();
-            
-            // the arrays count the respective occurrence of A, B, AB, and O
-            ArrayList<Integer> injured = new ArrayList<Integer>(4);
-            ArrayList<Integer> donors = new ArrayList<Integer>(4);
-            
-            for (int i = 0; i < 4; i++) {
-                injured.add(0);
-                donors.add(0);
-            }
-            
+
+            // The arrays count the respective occurrence of A, B, AB, and O
+            int[] injured = new int[4];
+            int[] donors = new int[4];
+
             for (int i = 0; i < n; i++) {
-                String s = scanner.next();
-                if (s.equals("A")) {
-                    injured.set(0, injured.get(0) + 1);
-                } else if (s.equals("B")) {
-                    injured.set(1, injured.get(1) + 1);
-                } else if (s.equals("AB")) {
-                    injured.set(2, injured.get(2) + 1);
-                } else if (s.equals("O")) {
-                    injured.set(3, injured.get(3) + 1);
-                }
+                String bloodType = scanner.next();
+                updateCount(bloodType, injured);
             }
-            
+
             for (int i = 0; i < n; i++) {
-                String s = scanner.next();
-                if (s.equals("A")) {
-                    donors.set(0, donors.get(0) + 1);
-                } else if (s.equals("B")) {
-                    donors.set(1, donors.get(1) + 1);
-                } else if (s.equals("AB")) {
-                    donors.set(2, donors.get(2) + 1);
-                } else if (s.equals("O")) {
-                    donors.set(3, donors.get(3) + 1);
-                }
+                String bloodType = scanner.next();
+                updateCount(bloodType, donors);
             }
-            
-            // we first check if we can satisfy the injured of blood type O
-            if (injured.get(3) <= donors.get(3)) {
-                donors.set(3, donors.get(3) - injured.get(3));
+
+            // Check if we can satisfy the injured of blood type O
+            if (injured[3] <= donors[3]) {
+                donors[3] -= injured[3];
             } else {
                 System.out.println("NO");
                 continue;
             }
-            
-            // then we check if we can satisfy the injured of blood type B
-            if (injured.get(1) <= donors.get(1) + donors.get(3)) {
-                donors.set(1, Math.max(donors.get(1) - injured.get(1), 0));
-                donors.set(3, donors.get(3) - Math.max(injured.get(1) - donors.get(1), 0));
+
+            // Check if we can satisfy the injured of blood type B
+            if (injured[1] <= donors[1] + donors[3]) {
+                int temp = donors[1];
+                donors[1] = Math.max(donors[1] - injured[1], 0);
+                injured[1] = Math.max(injured[1] - temp, 0);
+                donors[3] = donors[3] - injured[1];
             } else {
                 System.out.println("NO");
                 continue;
             }
-            
-            // then we check if we can satisfy the injured of blood type A
-            if (injured.get(0) <= donors.get(0) + donors.get(3)) {
-                donors.set(0, Math.max(donors.get(0) - injured.get(0), 0));
-                donors.set(3, donors.get(3) - Math.max(injured.get(0) - donors.get(0), 0));
+
+            // Check if we can satisfy the injured of blood type A
+            if (injured[0] <= donors[0] + donors[3]) {
+                int temp = donors[0];
+                donors[0] = Math.max(donors[0] - injured[0], 0);
+                injured[0] = Math.max(injured[0] - temp, 0);
+                donors[3] = donors[3] - injured[0];
             } else {
                 System.out.println("NO");
                 continue;
             }
-            
-            // if A, B, O are all satisfied then AB is satisfied
+
+            // If A, B, O are all satisfied, then AB is satisfied
             System.out.println("YES");
+        }
+    }
+
+    private static void updateCount(String bloodType, int[] countArray) {
+        switch (bloodType) {
+            case "A":
+                countArray[0]++;
+                break;
+            case "B":
+                countArray[1]++;
+                break;
+            case "AB":
+                countArray[2]++;
+                break;
+            case "O":
+                countArray[3]++;
+                break;
         }
     }
 }
--- Problem_09/AC_solution.py
+++ Problem_09/AC_solution.py
@@ -1,14 +1,13 @@
 t = int(input())
 for _ in range(t):
     n = int(input())
-
+    
     # the arrays count the respective occurrence of A, B, AB, and O
     injured = [0, 0, 0, 0]
     donors = [0, 0, 0, 0]
 
-    # Input for injured
-    blood_types = input().split()
-    for s in blood_types:
+    injured_input = input().split()
+    for s in injured_input:
         if s == "A":
             injured[0] += 1
         elif s == "B":
@@ -18,9 +17,8 @@ for _ in range(t):
         elif s == "O":
             injured[3] += 1
 
-    # Input for donors
-    blood_types = input().split()
-    for s in blood_types:
+    donors_input = input().split()
+    for s in donors_input:
         if s == "A":
             donors[0] += 1
         elif s == "B":
@@ -30,28 +28,32 @@ for _ in range(t):
         elif s == "O":
             donors[3] += 1
 
-    # we first check if we can satisfy the injured of blood type O
+    # check if we can satisfy the injured of blood type O
     if injured[3] <= donors[3]:
         donors[3] -= injured[3]
     else:
         print("NO")
         continue
 
-    # then we check if we can satisfy the injured of blood type B
+    # check if we can satisfy the injured of blood type B
     if injured[1] <= donors[1] + donors[3]:
+        temp = donors[1]
         donors[1] = max(donors[1] - injured[1], 0)
-        donors[3] -= max(injured[1] - donors[1], 0)
+        injured[1] = max(injured[1] - temp, 0)
+        donors[3] = donors[3] - injured[1]
     else:
         print("NO")
         continue
 
-    # then we check if we can satisfy the injured of blood type A
+    # check if we can satisfy the injured of blood type A
     if injured[0] <= donors[0] + donors[3]:
+        temp = donors[0]
         donors[0] = max(donors[0] - injured[0], 0)
-        donors[3] -= max(injured[0] - donors[0], 0)
+        injured[0] = max(injured[0] - temp, 0)
+        donors[3] = donors[3] - injured[0]
     else:
         print("NO")
         continue
 
-    # if A, B, O are all satisfied then AB is satisfied
+    # if A, B, O are all satisfied, then AB is satisfied
     print("YES")
